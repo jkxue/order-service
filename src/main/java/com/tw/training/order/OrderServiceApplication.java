0beb6acc197322afe775359d50c5ba1421cb0c07
jkxue <jkxupt@163.com>
2020-05-13 02:50:23 +0800

    fix: start failure

--- src/main/java/com/tw/training/order/OrderServiceApplication.java
+++ src/main/java/com/tw/training/order/OrderServiceApplication.java
@@ -7,18 +7,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 // 订单服务
 @SpringBootApplication
-@ComponentScan("com.tw")
 @EnableEurekaClient	
 @EnableCircuitBreaker // hystrix熔断
 @EnableFeignClients // feign
 @EnableTransactionManagement // 事务
-@EnableJpaRepositories(basePackages = { "com.tw.training" })
 public class OrderServiceApplication {
 	final static Logger logger = LoggerFactory.getLogger(OrderServiceApplication.class);
 
