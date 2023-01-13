8937485a1c804acc04b3f46804a38c24958bad85
jkxue <jkxupt@163.com>
2023-01-13 16:32:07 +0800

    feat: tracing with jaeger

--- src/main/java/com/tw/training/order/service/OrderService.java
+++ src/main/java/com/tw/training/order/service/OrderService.java
@@ -4,7 +4,6 @@ import com.tw.training.order.domain.OrderDomain;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.tw.training.order.client.WmsServiceClient;
@@ -14,9 +13,6 @@ import com.tw.training.order.repository.OrderRepository;
 @Transactional(rollbackFor=Throwable.class)
 public class OrderService {
 	static final Logger logger = LoggerFactory.getLogger(OrderService.class);
-
-	@Autowired
-	Tracer tracer;
 
 	@Autowired
 	OrderRepository orderRepository;
